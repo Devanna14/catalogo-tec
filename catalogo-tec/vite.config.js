71160101cd131223338c1a6d86a37582bb699698
Laraana <cecylar14@gmail.com>
2025-12-06 19:48:00 -0600

    Mover el proyecto a la raíz para GitHub Pages

--- catalogo-tec/vite.config.js
+++ catalogo-tec/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 💡 CLAVE: Establece la base path. Debe coincidir con el nombre de tu repositorio.
+  base: "/catalogo-tec/",
 })
